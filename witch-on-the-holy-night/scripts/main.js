97d3e5c3debc0970800f8df8ef0fae631db9c1e8
x1010011010 <2142196929@qq.com>
2023-12-23 12:11:12 +0800

    Update main.js

--- witch-on-the-holy-night/scripts/main.js
+++ witch-on-the-holy-night/scripts/main.js
@@ -16,8 +16,12 @@ let myHeading = document.querySelector("h1");
 
 function setUserName() {
   let myName = prompt("请输入你的名字。");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = "Welcome，" + myName + "!";
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+  }
 }
 if (!localStorage.getItem("name")) {
   setUserName();
